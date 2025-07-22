abc4f7716ba9156bb14f709dbc09361ed3a5d347
Torwent <Torwent@users.noreply.github.com>
2025-07-22 16:05:13 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,31 @@
+import { mdsvex, escapeSvelte } from "mdsvex"
 import adapter from "@sveltejs/adapter-node"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
+import { getSingletonHighlighter } from "shiki"
+
+const shikiHighlighter = await getSingletonHighlighter({
+	themes: ["github-dark", "github-light"],
+	langs: ["javascript", "typescript", "bash", "cmd", "yml", "yaml", "pascal", "java", "json"]
+})
+
+const mdsvexOptions = {
+	extensions: [".md"],
+	highlight: {
+		highlighter: async (code, lang = "text") => {
+			if (lang === "freepascal") lang = "pascal"
+			const html = escapeSvelte(
+				shikiHighlighter.codeToHtml(code, {
+					lang,
+					themes: { light: "github-light", dark: "github-dark" }
+				})
+			)
+			return `{@html \`${html}\` }`
+		}
+	}
+}
 
 const config = {
-	preprocess: [vitePreprocess()],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 	kit: {
 		adapter: adapter(),
 		csp: {
@@ -34,7 +57,7 @@ const config = {
 			}
 		}
 	},
-	extensions: [".svelte"]
+	extensions: [".svelte", ".svx", ".md"]
 }
 
 export default config
