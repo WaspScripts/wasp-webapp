69bbe78b1e2122829051d24490103f7f9daaf659
Torwent <Torwent@users.noreply.github.com>
2026-02-07 09:43:06 +0100

    add launcher successful/failed auth pages

---
+++ src/routes/auth/launcher/failed/+page.svelte
@@ -0,0 +1,7 @@
+<main class="flex h-screen flex-col">
+	<div class="mx-auto my-44 text-center">
+		<h1 class="my-12 h1">Authentication Failed</h1>
+
+		<p class="text-xl">Something went wrong!</p>
+	</div>
+</main>
---
+++ src/routes/auth/launcher/successful/+page.svelte
@@ -0,0 +1,7 @@
+<main class="flex h-screen flex-col">
+	<div class="mx-auto my-44 text-center">
+		<h1 class="my-12 h1">Authentication Complete</h1>
+
+		<p class="text-xl">You may now close this window and return to the launcher!</p>
+	</div>
+</main>
